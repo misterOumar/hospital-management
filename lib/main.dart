71e8488a55f528989b2a41e67773902902e1e530
MiscellFox <116920581+MiscellFox@users.noreply.github.com>
2023-08-13 19:52:46 +0000

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:hospital_dashboard/models/patients.dart';
 import 'package:hospital_dashboard/screens/main/main_screen.dart';
 import 'constants.dart';
+import 'models/boxes.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(PersonAdapter());
+  boxPersonnes = await Hive.openBox<Person>('PatientBox');
+  runApp(MyApplication());
 }
 
-class MyApp extends StatelessWidget {
+class MyApplication extends StatelessWidget {
   // This widget is the root of your application.
 
   @override
